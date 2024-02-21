6866a23ff05e4d887c28eec688a6e7407b35b5d4
현미 <akshwpsh@gmail.com>
2024-02-22 04:40:58 +0900

    #feat: 업로드
    Signed-off-by: 현미 <akshwpsh@gmail.com>

---
+++ Source/FirstTPS/FirstTPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstTPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFirstTPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFirstTPSGameMode();
+};
+
+
+
